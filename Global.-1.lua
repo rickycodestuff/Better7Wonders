55f91815af366e35b34d6c88746afcd29cc773cb
Riccardo Di Michele <rickycodestuff@gmail.com>
2023-06-14 03:35:04 +0200

    added a dynamic status panel

--- Global.-1.lua
+++ Global.-1.lua
@@ -782,8 +782,8 @@ function populateStatusPanel()
     -- the goal for this function is to populate the status UI everytime a player joins or changes color
     -- inside our Global.xml we already have a <TableLayout> inside a <Panel> ready to be populated
 
-    -- for every n players we will "build" a n numbers of rows to "append" to that Panel xml element
-    -- the xml schema to append will be as follows
+    -- for every n players we will "build" n rows to collect inside a table 
+    -- the xml schema for every row will be as follow
     -- ? <Row>
     -- ?     <Cell>
     -- ?          <Panel class = 'statusBox'>
@@ -794,119 +794,92 @@ function populateStatusPanel()
     -- ?     </Cell>
     -- ? </Row>
 
-    -- we'll have to parse that into a lua table with the following schema
-    -- ? local row_to_append = 
-    -- ? {
-    -- ?     tag = 'Row',
-    -- ?     attributes = {},                                -- ! <Row>
-    -- ?     children = 
-    --?      {
-    -- ?         tag = 'Cell',
-    -- ?         attributes = {},                                -- ! <Cell>
-    -- ?         children = 
-    -- ?         {
-    -- ?             tag = 'Panel',
-    -- ?             attributes = { class = 'statusBox' },           -- ! <Panel class = 'statusBox'></Panel>
-    -- ?             children = {}
-    -- ?         }
-    -- ?     },                                                  -- ! </Cell>
-    -- ?     {
-    -- ?         tag = 'Cell',
-    -- ?         attributes = {},                                -- ! <Cell>
-    -- ?         children = 
-    -- ?         {
-    -- ?             tag = 'Text',
-    -- ?             attributes = { class = 'playerName' },          -- ! <Text>player.steam_name</Text>
-    -- ?             value = player.steam_name
-    -- ?         }
-    -- ?     }                                                   -- ! </Cell>
-    -- ? }                                                   -- ! </Row>
+    -- once we've built all the rows we'll "overwrite" the current children of <TableLayout>
+    -- and not append the new children to the old ones
+    -- otherwise the status panel will append every new row without removing the old ones
 
     -- ! real function
-    -- start reading this from the title_row table
+    -- all the tags inside Global.xml
+    local xml_table = self.UI.getXmlTable()
 
-    -- ? <Text> 
-    local title_text = {
-        tag = 'Text',
-        attributes = {id = 'statusTitle', class = 'statusTitle'},
-        value = 'WAITING TO START MATCH',
-        children = {}
-    }
+    -- this we'll be our "collection" of all the row tags
+    -- one row (=table) for each player in the match
+    local new_children = {}
 
-    -- ? <Row> 
-    local title_row = {
-        tag = 'Row',
-        attributes = {},
-        children = {title_text}
-    }
-
-    -- the two tables above are just a parent and a children tag, like so:
-    -- ? <Row>
-    -- ?      <Text id = 'statusTitle' class = 'statusTitle'>WAITING TO START MATCH</Text>
-    -- ? </Row>
-
-    -- this we'll be our "collection" of all the row tag to append
-    -- one row for each player in the match
     -- except for the first table, it will be the title of our panel
-    local xml_to_append = {title_row}
+    new_children[1] = xml_table[2]      -- the second child of Global.xml that is <Panel>    
+                        .children[1]    -- the first child of <Panel> that is <TableLayout>
+                        .children[1]    -- the first child of <TableLayout> that is the title row
 
+    -- now for each player we will generate a row tag
+    -- containing his status box and his steam name 
     for _, player in pairs(getValidPlayers()) do
 
-        -- start reading this from the last table, the row_tag trust me 
+        -- start reading this from the last table, the row_player  
 
         -- the player name in question
-        local player_text = {
+        local text_player_name = {
             tag            = 'Text',
-            attributes     = { class = 'playerName' },
-            value          = player.color,
+            attributes     = {class = 'textPlayerName'},
+            value          = player.color, -- TODO change to steam_name 
             children       = {}
         }
 
         -- and this is the second and last child of the <Row>
         -- this <Cell> contains that player's name, it will be displayed next to his status
-        local name_cell = {
+        local cell_player_name = {
             tag            = 'Cell',
             attributes     = {},
-            children       = {player_text}
+            children       = {text_player_name}
         }
 
         -- the status box in question
-        local status_panel = {
+        local panel_player_status = {
             tag            = 'Panel',
-            attributes     = { class = 'statusBox' },
+            attributes     = {class = 'panelPlayerStatus'},
             children       = {}
         }
 
         -- this is the first child tag, the <Cell> containg the status box
         -- this box will display whenever a player is ready or not
-        local status_cell = {
+        local cell_player_status = {
             tag            = 'Cell',
             attributes     = {},
-            children       = {status_panel}
+            children       = {panel_player_status}
         }
 
         -- this the parent tag <Row> that contains every other table you see above this 
-        local player_row = {
+        local row_player = {
             tag            = 'Row',
-            attributes     = { height = '32' },
-            children       = {status_cell, name_cell}
+            attributes     = {},
+            children       = {cell_player_status, cell_player_name}
         }
 
-        xml_to_append[#xml_to_append + 1] = player_row
+        -- after building this tag we'll append to our collection
+        new_children[#new_children + 1] = row_player
     end
 
-    -- since it's not possibile to "append" the current Global.xml we first store it
-    local global_xml = self.UI.getXmlTable()
+    -- at the end of the loop we'll overwrite the table's children
+    -- same strucutre as before 
+    xml_table[2].children[1].children = new_children
 
-    -- and then we overwrite it by adding these new rows
-    -- more more specifically: 
-    -- global_xml[2] -> is the second parent tag in Global.xml that is <Panel>
-    -- .children[1]  -> refers to the first children tag of <Panel> that is <TableLayout>
-    -- .children     -> this are all the children of <TableLayout> so an empty table
-    global_xml[2].children[1].children = xml_to_append
+    -- lastly we set and update the panel using the LuaCoroutine
+    self.UI.setXmlTable(xml_table)
+    updateStatusPanelUI(#getValidPlayers())
 
-    -- finally we set this dynamic xml table which will serve us for the entire runtime of the match
-    self.UI.setXmlTable(global_xml)
+    -- TODO remove comment 
+    -- local button_ready = {
+    --     tag = 'Button',
+    --     attributes = {class = 'buttonReady'},
+    --     value = 'Ready',
+    --     children = {}
+    -- }
+
+    -- local row_ready = {
+    --     tag = 'Row',
+    --     attributes = {preferredHeight = '64'},
+    --     children = {button_ready}
+    -- }
 
 end
 
@@ -914,11 +887,10 @@ function getValidPlayers()
     -- the tabletop api only gives u the Player.getPlayers() function wich will return a table of Player instances
     -- but we only want the players having a "valid colors" 
     -- so not players in the grey nor black seat
-
     local valid_colors = {'White', 'Purple', 'Red', 'Yellow', 'Green', 'Orange', 'Blue'}
     local valid_players = {}
 
-    -- we compare every player's color with every valid colors and just append one that matches
+    -- we compare every player's color with every valid colors and just append the ones that matches
     for _, player in pairs(Player.getPlayers()) do
         for _, color in pairs(valid_colors) do
             if player.color == color then
@@ -930,6 +902,48 @@ function getValidPlayers()
     -- this is a Player instances table
     -- its pretty useful because we can get the steam name, the steam id, the color and many other things
     return valid_players
+end
+
+-- since the startLuaCoroutine() function does not let you use paramaters
+-- we use this first function as the "outside" of the coroutine function
+-- thanks to it we can use its paramater in the "inside"
+function updateStatusPanelUI(n_players)
+    function coinside()
+
+        -- pauses the couritine to resolve the UI
+        coroutine.yield(0)
+
+        -- to "visually" add a row inside our panel we have to edit its height, using:
+        -- 1) each row height
+        -- 2) the spacing between each one of them
+        -- 3) the height of the panel itself
+        -- 4) the padding of the panel
+        local row_height = tonumber(UI.getAttribute('rowPlayer', 'preferredHeight'))
+        local table_spacing = tonumber(UI.getAttribute('tableStatus', 'cellSpacing'))
+        local panel_height = tonumber(UI.getAttribute('panelStatus', 'height'))
+        local panel_padding = tonumber(UI.getAttribute('panelStatus', 'padding'))
+
+        -- this is the "minum" size of our panel, meaning the size needed to only contain the title
+        local new_panel_height = panel_padding + row_height + table_spacing
+
+        -- and we add to it the new player rows
+        new_panel_height = new_panel_height + (row_height + table_spacing) * n_players
+
+        if new_panel_height ~= panel_height then
+            -- update panel height
+            UI.setAttribute('panelStatus', 'height', new_panel_height)
+        end
+
+    return 1
+    end
+
+    -- the updateStatusPanelUI() will ignore everything in coinside() and jump right into this 
+    -- as you can see this is the real function called by the LuaCoroutine
+    startLuaCoroutine(self, 'coinside')
+
+    -- if you are wondering why using a LuaCoroutine to do this it's because (as the docs says)
+    -- ! "UI changes do not take effect immediately. Any attempt to query the contents of the XML will return stale results"
+    -- the LuaCoroutine generates a new "thread" to "resolve" the UI changes
 end
 
 function onChat(message, player)
